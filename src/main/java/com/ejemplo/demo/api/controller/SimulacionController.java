a7538d78df4decbf2e2cfff7ce567c0041be0026
Josue Paniagua <jpaniaguao@miumg.edu.gt>
2026-04-08 16:46:10 -0600

    Update endpoint prestamos paso 8

--- src/main/java/com/ejemplo/demo/api/controller/SimulacionController.java
+++ src/main/java/com/ejemplo/demo/api/controller/SimulacionController.java
@@ -3,19 +3,14 @@ package com.ejemplo.demo.api.controller;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ejemplo.demo.api.dto.PrestamoRequest;
 import com.ejemplo.demo.api.dto.PrestamoResponse;
-import com.ejemplo.demo.api.dto.SaludoRequest;
-import com.ejemplo.demo.api.dto.SaludoResponse;
 import com.ejemplo.demo.domain.service.PrestamoService;
-import com.ejemplo.demo.domain.service.SaludoService;
 
 import jakarta.validation.Valid;
 
